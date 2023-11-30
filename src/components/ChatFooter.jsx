05f3f86b17d963b8ea86b7974c768705f68e82d0
jaydenants <jsanch871@west-mec.org>
2023-11-30 13:25:42 -0700

    first commit

---
+++ src/components/ChatFooter.jsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+import {baseURL} from '../util';
+
+
+const ChatFooter = ({openChat, socket, user, setMessages})=>{
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+
+        const newMessage = {
+            sender: user._id,
+            message,
+            chatId: openChat.chatId
+        }
+
+        let receivers = openChat.members.filter((memberId)=> memberId !== user._id);
+       
+
+        socket.current.emit("sendMessage",{
+            sender: user._id,
+            receivers,
+            message
+        })
+
+        await axios.post(`${baseURL}/users/message`, newMessage).then((res)=>{
+            console.log(res.data.message);
+            setMessages((prev)=> [...prev, res.data.message]);
+        }).catch(err=> console.log(err))
+        setMessage("");
+    }
+
+    return(
+        <div className='chat-footer'>
+            <form className='form' onSubmit={handleSubmit}>
+                <input
+                type="text"
+                placeholder="Write message"
+                className='message'
+                value={message}
+                onChange={e=> setMessage(e.target.value)}
+                />
+                <button className="sendBtn"><i className="bi bi-send-fill"></i></button>
+            </form>
+        </div>
+    )
+}
+
+export default ChatFooter
